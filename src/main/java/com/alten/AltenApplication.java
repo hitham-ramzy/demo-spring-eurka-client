c472cb8556f4f3e20d40012faa4063b94e762cd6
hitham <hitham.ramzy@mondiamedia.com>
2018-12-23 12:41:24 +0200

    Adding and defining eureka client

--- src/main/java/com/alten/AltenApplication.java
+++ src/main/java/com/alten/AltenApplication.java
@@ -2,6 +2,8 @@ package com.alten;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -14,6 +16,8 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 @EnableSwagger2
 @Configuration
